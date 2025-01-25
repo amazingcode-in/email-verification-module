c150136f916f6815c74c85b3a24318f1f7e2c166
amazingcode-in <amazingcode.in@gmail.com>
2025-01-25 10:52:26 +0530

    project created

---
+++ src/main/java/com/amazingcode/in/example/EmailVerificationModuleApplication.java
@@ -0,0 +1,11 @@
+package com.amazingcode.in.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationModuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailVerificationModuleApplication.class, args);
+    }
+}
